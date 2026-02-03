00536214bc65e8641329e4dbe18ef9acadae397d
Amyt16020 <amyt16020@gmail.com>
2026-02-03 21:42:59 +0800

    textrecognize.py

---
+++ textrecognize.py
@@ -0,0 +1,17 @@
+import cv2
+
+gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
+_, bw = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)
+
+contours, _ = cv2.findContours(
+    bw, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+)
+
+for c in contours:
+    x, y, w, h = cv2.boundingRect(c)
+
+    if w > 20 and h > 40:
+        cv2.rectangle(frame, (x, y), (x+w, y+h),(255, 0, 0), 2)
+
+cv2.imshow("auto detect", frame)
